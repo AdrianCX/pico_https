a65da9a56f8b930b637d3074d59b5860be835e77
Adrian Cruceru <cruceruadrian@gmail.com>
2022-09-27 19:00:13 +0200

    Fix script

--- pico_tls/certificate/create_cert.sh
+++ pico_tls/certificate/create_cert.sh
@@ -1,8 +1,8 @@
 #!/bin/bash -ex
 
 # This generates a new self signed certificate for testing
-openssl ecparam -name prime256v1 -genkey -noout -outform DER -out key.der
-openssl req -new -x509 -key key.der -outform DER -out cert.der -days 360
+openssl ecparam -name prime256v1 -genkey -noout -outform DER -out ./key.der
+openssl req -new -x509 -key ./key.der -outform DER -out ./cert.der -days 360
 
 if [ -z "$1" ]; then
     TARGET_FOLDER=$(pwd)
@@ -20,7 +20,7 @@ echo "#define _KEY_H" >> "$TARGET_FOLDER/key.h"
 xxd -i ./key.der >> "$TARGET_FOLDER/key.h"
 echo "#endif" >> "$TARGET_FOLDER/key.h"
 
-rm -f cert.der key.der
+rm -f ./cert.der ./key.der
 
 # You can convert existing certificates from pem to der via line below then use xxd: (ec or rsa based on what you have)
 # openssl ec -in ./key.pem -outform DER -out ./key.der
